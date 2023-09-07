0e0177b028beb4885e6ceb05883c38cea2914e2b
noahring <noah.ring@centre.edu>
2023-09-07 16:09:14 +0000

    Finished Lab 02

--- lab02b.sql
+++ lab02b.sql
@@ -17,13 +17,13 @@ USE school;
 CREATE TABLE instructors (
     PRIMARY KEY (instructor_id),
     instructor_id      INT AUTO_INCREMENT,
-    inst_first_name    VARCHAR(20),
-    inst_last_name     VARCHAR(20),
+    instfirst_name    VARCHAR(20),
+    instlast_name     VARCHAR(20),
     campus_phone       VARCHAR(20)
 );
 
 /* Populate the instructors table with sample data */
-INSERT INTO instructors (inst_first_name, inst_last_name, campus_phone)
+INSERT INTO instructors (instfirst_name, instlast_name, campus_phone)
 VALUES ( 'Kira'   , 'Bentley', '363-9948' ),
        ( 'Timothy', 'Ennis'  , '527-4992' ),
        ( 'Shannon', 'Black'  , '336-5992' ),
