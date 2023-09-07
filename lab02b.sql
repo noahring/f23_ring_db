fd1d1dd522ea0294333ac322289849e4db118620
noahring <noah.ring@centre.edu>
2023-09-07 16:04:50 +0000

    Finished Lab 02

---
+++ lab02b.sql
@@ -0,0 +1,36 @@
+/*
+    Lab 2: MariaDB Tutorial
+    CSC 362 Database Systems
+    Noah Ring
+*/
+
+/* Create the database */
+
+DROP DATABASE IF EXISTS school;
+
+CREATE DATABASE school;
+
+USE school;
+
+/* Creating instructors table */
+
+CREATE TABLE instructors (
+    PRIMARY KEY (instructor_id),
+    instructor_id      INT AUTO_INCREMENT,
+    inst_first_name    VARCHAR(20),
+    inst_last_name     VARCHAR(20),
+    campus_phone       VARCHAR(20)
+);
+
+/* Populate the instructors table with sample data */
+INSERT INTO instructors (inst_first_name, inst_last_name, campus_phone)
+VALUES ( 'Kira'   , 'Bentley', '363-9948' ),
+       ( 'Timothy', 'Ennis'  , '527-4992' ),
+       ( 'Shannon', 'Black'  , '336-5992' ),
+       ( 'Estela' , 'Rosales', '322-6992' );
+
+/* Displays all data in the table. */
+
+SELECT * FROM instructors
+
+/* End of file lab02b.sql */
